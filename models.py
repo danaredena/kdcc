86d24b788c8b7caa71c747dd70584fd8166ffcac
danaredena <dana.redena@gmail.com>
2017-01-17 19:20:52 +0800

    database initial

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
-from sqlalchemy import create_engine
+from sqlalchemy import *
+#from sqlalchemy import Column, String
+#from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -9,6 +10,33 @@ class User(Base):
 
     username = Column(String(20), nullable=False, primary_key=True)
     password = Column(String(20), nullable=False)
+
+class Students(Base):
+    __tablename__ = 'students'
+
+    studentID = Column(Integer, nullable=False, primary_key=True)
+    firstName = Column(String(70), nullable=False)
+    lastName = Column(String(50), nullable=False)
+    birthDate = Column(String(10), nullable=False)
+    age = Column(Integer, nullable=False)
+    address = Column(String, nullable=False)
+    group = Column(String(20), nullable=False)
+
+class Faculty(Base):
+    __tablename__ = 'faculty'
+
+    facultyID = Column(Integer, nullable=False, primary_key=True)
+    firstName = Column(String(70), nullable=False)
+    lastName = Column(String(50), nullable=False)
+    birthDate = Column(String(10), nullable=False)
+    address = Column(String, nullable=False)
+    contactNumber = Column(String(11), nullable=False)
+
+
+class Semester(Base):
+    __tablename__ = 'semester'
+
+    semCode = Column(String(10), nullable=False, primary_key=True)
 
 
 engine = create_engine('sqlite:///kdcc.db')
