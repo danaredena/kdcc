89c5a5f340e3de329c364227a345f9f04e3bbd55
Eugene Kasilag <eugene_dose@yahoo.com>
2017-01-24 01:01:38 +0800

    create student class

--- main.py
+++ main.py
@@ -12,7 +12,22 @@ DBSession = sessionmaker()
 DBSession.configure(bind=engine)
 session = DBSession()
 
-
+class Student():
+    def __init__(self):
+        self.nickname = None
+        self.first_name = None
+        self.middle_name = None
+        self.last_name = None
+        self.address = None
+        self.birthday = None
+        self.sex = None
+        self.date_of_enrollment = None
+        self.group = None
+        self.guardian1_name = None
+        self.guardian2_name = None
+        self.contact_number1 = None
+        self.contact_number2 = None
+        self.UP_NonUP = None
 
 class LoginWindow(Widget):
     def login(self, *args):
@@ -45,6 +60,23 @@ class StudentRecordsWindow(Widget):
         self.add_widget(CreateWindow())
 
 class CreateWindow(Widget):
+    def create(self, *args):
+        student.nickname = self.ids.nickname
+        student.first_name = self.ids.first_name
+        student.middle_name = self.ids.middle_name
+        student.last_name = self.ids.last_name
+        student.address = self.ids.address
+        student.birthday = self.ids.birthday
+        student.sex = self.ids.sex
+        student.date_of_enrollment = self.ids.date_of_enrollment
+        student.group = self.ids.group
+        student.guardian1_name = self.ids.guardianA
+        student.guardian2_name = self.ids.guardianB
+        student.contact_number1 = self.ids.contactA
+        student.contact_number2 = self.ids.contactB
+        student.UP_NonUP = self.ids.up_nonup
+        self.clear_widgets()
+        self.add_widget(StudentRecordsWindow())
     def back_to_student_records(self, *args):
         self.clear_widgets()
         self.add_widget(StudentRecordsWindow())
@@ -53,6 +85,7 @@ class KDCCApp(App):
     def build(self):
         return LoginWindow()
 
+student = Student()
 
 if __name__ == '__main__':
     KDCCApp().run()
