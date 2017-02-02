6565233643ff8a5e492c5264ff4e4f3ca36ba657
danaredena <dana.redena@gmail.com>
2017-02-02 10:00:27 +0800

    final student database

--- models.py
+++ models.py
@@ -14,41 +14,49 @@ class User(Base):
 class Students(Base):
     __tablename__ = 'student'
 
-    studentID = Column(Integer, nullable=False, primary_key=True)
-    firstName = Column(String(70), nullable=False)
-    lastName = Column(String(50), nullable=False)
-    birthDate = Column(String(10), nullable=False)
-    age = Column(Integer, nullable=False)
+    student_id = Column(Integer, nullable=False, primary_key=True)
+    first_name = Column(String(70), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    middle_name = Column(String(50), nullable=False)
+    nickname = Column(String(30), nullable=False)
+    birth_date = Column(String(10), nullable=False)
+    #age = Column(Integer, nullable=False) #autocompute na age
     address = Column(String, nullable=False)
-    group = Column(String(20), nullable=False)
+    group = Column(String(20), nullable=True)
+    date_of_admission = Column(String, nullable=False)
+    guardian1_name = Column(String, nullable=False)
+    contact_number1 = Column(String, nullable=False)
+    guardian2_name = Column(String, nullable=True)
+    contact_number2 = Column(String, nullable=True)
+    up_dependent = Column(Boolean, nullable=False)
 
 class Faculty(Base):
     __tablename__ = 'faculty'
 
-    facultyID = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    firstName = Column(String(70), nullable=False)
-    lastName = Column(String(50), nullable=False)
-    birthDate = Column(String(10), nullable=False)
+    faculty_id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    first_name = Column(String(70), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    birth_date = Column(String(10), nullable=False)
     address = Column(String, nullable=False)
-    contactNumber = Column(String(11), nullable=False)
+    contact_number = Column(String(11), nullable=False)
 
 
 class Semester(Base):
     __tablename__ = 'semester'
 
-    semCode = Column(String(10), nullable=False, primary_key=True)
+    sem_code = Column(String(10), nullable=False, primary_key=True)
 
 class Enrolled(Base):
     __tablename__ = 'enrolled'
-    semCode = Column(String(10), nullable=False, primary_key=True)
-    studentID = Column(Integer, nullable=False, primary_key=True)
-    ForeignKeyConstraint(['semCode', 'studentID'], ['semester.semCode', 'student.studentID'])
+    sem_code = Column(String(10), nullable=False, primary_key=True)
+    student_id = Column(Integer, nullable=False, primary_key=True)
+    ForeignKeyConstraint(['sem_code', 'student_id'], ['semester.sem_code', 'student.student_id'])
 
 class Employed(Base):
     __tablename__ = 'employed'
-    semCode = Column(String(10), nullable=False, primary_key=True)
-    facultyID = Column(Integer, nullable=False, primary_key=True)
-    ForeignKeyConstraint(['semCode', 'facultyID'], ['semester.semCode', 'faculty.facultyID'])
+    sem_code = Column(String(10), nullable=False, primary_key=True)
+    faculty_id = Column(Integer, nullable=False, primary_key=True)
+    ForeignKeyConstraint(['sem_code', 'faculty_id'], ['semester.sem_code', 'faculty.faculty_id'])
 
 
 engine = create_engine('sqlite:///kdcc.db')
