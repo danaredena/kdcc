c3e216a789a3fc373f1c6bde9112efa8e2179782
danaredena <dana.redena@gmail.com>
2017-02-08 00:04:57 +0800

    faculty create fixed

--- kdcc_record.py
+++ kdcc_record.py
@@ -11,9 +11,9 @@ DBSession = sessionmaker()
 DBSession.configure(bind=engine)
 
 def fill():
-    session = DBSession()
     try:
         #1: ADD USER 'admin'
+        session = DBSession()
         new_user = User(username='admin', password='admin')
         session.add(new_user)
         new_user = User(username='guest', password='guest')
@@ -21,10 +21,16 @@ def fill():
         session.commit()
 
         #2: ADD STUDENT (arbitrary) 'Diane'
+        session = DBSession()
         new_student = Students(nickname='Diane', first_name='Diane', middle_name='Yap', last_name='Red', address='Tandang Sora, Quezon City', birth_date='11/07/2010', sex='female', date_of_admission='02/01/2017', group='toddler', guardian1_name='Scarlett Red', contact_number1='09431234567', up_dependent='yes')
         session.add(new_student)
         session.commit()
 
+        #3: ADD FACULTY (arbitrary) 'Bob'
+        session = DBSession()
+        new_student = Faculty(id_number = 909, first_name='Bob', middle_name='Dash', last_name='Parr', address='pixar', birth_date='04/09/1973', sex='male', date_of_employment='03/07/2015', position='Rogue', contact_number='09761234567', pers_tin='9302', pers_ssn='93043', pers_philhealth='340393', pers_accntnum='3094920')
+        session.add(new_student)
+        session.commit()
     except: #this part ensures na no drastic changes will happen in case something goes wrong
         session.rollback()
         raise
--- main.py
+++ main.py
@@ -51,13 +51,14 @@ class Facuty():
         self.birth_date = None
         self.age = None #autocompute
         self.sex = None
-        self.date_of_admission = None
-        self.group = None
-        self.guardian1_name = None
-        self.guardian2_name = None
-        self.contact_number1 = None
-        self.contact_number2 = None
-        self.up_dependent = None
+        self.date_of_employment = None
+        self.position = None
+        self.contact_number = None
+        self.pers_tin = None
+        self.pers_ssn = None
+        self.pers_philhealth = None
+        self.pers_accntnum = None
+        self.remarks = None
 
 class LoginWindow(Widget):
     def login(self, *args):
@@ -147,7 +148,7 @@ class CreateStudentWindow(Widget):
 
         new_student = Students(nickname=nickname_text, first_name=first_name_text, middle_name=middle_name_text, last_name=last_name_text, address=address_text, birth_date=birth_date_text, sex=sex_text, date_of_admission=date_of_admission_text, group=group_text, guardian1_name=guardian1_name_text, guardian2_name=guardian2_name_text, contact_number1=contact_number1_text, contact_number2=contact_number2_text, up_dependent=up_dependent_text)
         print( add_db(new_student) )
-        
+
         #print(session.query(Students).all())
         self.clear_widgets()
         self.add_widget(StudentRecordsWindow())
@@ -161,54 +162,55 @@ class CreateFacultyWindow(Widget):
         #nickname_text = faculty.nickname.text
 
         faculty.id_number = self.ids.id_number
-        id_number_text = faculty.id_number.text
+        f_id_number_text = faculty.id_number.text
 
         faculty.first_name = self.ids.first_name
-        first_name_text = faculty.first_name.text
+        f_first_name_text = faculty.first_name.text
 
         faculty.middle_name = self.ids.middle_name
-        middle_name_text = faculty.middle_name.text
+        f_middle_name_text = faculty.middle_name.text
 
         faculty.last_name = self.ids.last_name
-        last_name_text = faculty.last_name.text
+        f_last_name_text = faculty.last_name.text
 
         faculty.address = self.ids.address
-        address_text = faculty.address.text
+        f_address_text = faculty.address.text
 
         faculty.birth_date = self.ids.birth_date
-        birth_date_text = faculty.birth_date.text
+        f_birth_date_text = faculty.birth_date.text
 
         faculty.sex = self.ids.sex
-        sex_text = faculty.sex.text
+        f_sex_text = faculty.sex.text
 
         faculty.date_of_employment = self.ids.date_of_employment
-        date_of_employment_text = faculty.date_of_employment.text
+        f_date_of_employment_text = faculty.date_of_employment.text
 
         faculty.position = self.ids.position
-        position_text = faculty.position.text
+        f_position_text = faculty.position.text
 
         faculty.contact_number = self.ids.contact_number
-        contact_number = faculty.contact_number.text
+        f_contact_number_text = faculty.contact_number.text
 
         faculty.pers_tin = self.ids.tin_number
-        pers_tin_text = faculty.pers_tin.text
+        f_pers_tin_text = faculty.pers_tin.text
 
         faculty.pers_ssn = self.ids.social_security_number
-        pers_ssn_text = faculty.pers_ssn.text
+        f_pers_ssn_text = faculty.pers_ssn.text
 
         faculty.pers_philhealth = self.ids.philhealth
-        pers_philhealth_text = faculty.pers_philhealth.text
+        f_pers_philhealth_text = faculty.pers_philhealth.text
 
         faculty.pers_accntnum = self.ids.account_number
-        pers_accntnum_text = faculty.pers_accntnum.text
+        f_pers_accntnum_text = faculty.pers_accntnum.text
 
         faculty.remarks = self.ids.remarks
-        remarks_text = faculty.remarks.text
+        f_remarks_text = faculty.remarks.text
 
+        new_faculty = Faculty(id_number = f_id_number_text, first_name=f_first_name_text, middle_name=f_middle_name_text, last_name=f_last_name_text, address=f_address_text, birth_date=f_birth_date_text, sex=f_sex_text, date_of_employment=f_date_of_employment_text, position=f_position_text, contact_number=f_contact_number_text, pers_tin=f_pers_tin_text, pers_ssn=f_pers_ssn_text, pers_philhealth=f_pers_philhealth_text, pers_accntnum=f_pers_accntnum_text, remarks=f_remarks_text)
+        print(new_faculty.id_number, new_faculty.first_name, new_faculty.middle_name, new_faculty.last_name, new_faculty.address, new_faculty.birth_date, new_faculty.sex, new_faculty.date_of_employment, new_faculty.position, new_faculty.contact_number, new_faculty.pers_tin, new_faculty.pers_ssn, new_faculty.pers_philhealth, new_faculty.pers_accntnum, new_faculty.remarks)
+        print( add_db(new_faculty) )
 
-        new_faculty = Faculty(id_number=id_number_text, first_name=first_name_text, middle_name=middle_name_text, last_name=last_name_text, address=address_text, birth_date=birth_date_text, sex=sex_text, date_of_employment=date_of_employment_text, position=position_text, contact_number=contact_number_text, tin_number=tin_number_text, social_security_number=social_security_number_text, philhealth=philhealth_text, account_number=account_number_text, remarks=remarks_text)
-        print( add_db(faculty) )
-        
+        #print(f_id_number_text, f_first_name_text, f_middle_name_text, f_last_name_text, f_address_text, f_birth_date_text, f_sex_text, f_date_of_employment_text, f_position_text, f_contact_number_text, f_pers_tin_text, f_pers_ssn_text, f_pers_philhealth_text, f_pers_accntnum_text, f_remarks_text)
         self.clear_widgets()
         self.add_widget(FacultyRecordsWindow())
     def back_to_faculty_records(self, *args):
@@ -220,6 +222,6 @@ class KDCCApp(App):
         return LoginWindow()
 
 student = Student()
-
+faculty = Faculty()
 if __name__ == '__main__':
     KDCCApp().run()
