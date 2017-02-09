dde713f9897c3f09f580e9741f78a44d2decb99e
danaredena <dana.redena@gmail.com>
2017-02-09 08:59:16 +0800

    binalik mga nadelete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
@@ -102,6 +103,7 @@ class StudentListButton(ListItemButton):
 class StudentRecordsWindow(Widget):
     student_list = ObjectProperty()
     def populate_list(self):
+        del self.student_list.adapter.data[:]
         students = Students.query.all()
         for student in students:
             details = [str(student.student_id), student.nickname, student.first_name+' '+student.middle_name+' '+student.last_name, student.group, student.birth_date, student.address, student.up_dependent, student.date_of_admission]
@@ -123,6 +125,10 @@ class StudentRecordsWindow(Widget):
 
             delete_db(int(selection[0]), 0) #gets student_id, 0 - for student record
             self.student_list._trigger_reset_populate()
+    def choose_semester(self, *args):
+        self.clear_widgets()
+        self.add_widget(ChooseSemesterWindow())
+
 
 class CreateStudentWindow(Widget):
     def create(self, *args):
@@ -178,6 +184,19 @@ class CreateStudentWindow(Widget):
         self.clear_widgets()
         self.add_widget(StudentRecordsWindow())
 
+class ChooseSemesterWindow(Widget):
+    def main_menu(self, *args):
+        self.clear_widgets()
+        self.add_widget(MainMenuWindow())
+    def back_to_student_records(self, *args):
+          self.clear_widgets()
+          self.add_widget(StudentRecordsWindow())
+
+class SemesterListWindow(Widget):
+    def choose_semester(self, *args):
+        self.clear_widgets()
+        self.add_widget(ChooseSemesterWindow())
+
 class Facuty():
     def __init__(self):
         self.id_number = None
@@ -203,6 +222,7 @@ class FacultyListButton(ListItemButton):
 class FacultyRecordsWindow(Widget):
     faculty_list = ObjectProperty()
     def populate_list(self):
+        del self.faculty_list.adapter.data[:]
         all_faculty = Faculty.query.all()
         for faculty in all_faculty:
             details = [str(faculty.faculty_id), faculty.id_number, faculty.first_name+' '+faculty.middle_name+' '+faculty.last_name, faculty.position, faculty.contact_number, faculty.birth_date, faculty.date_of_employment]
@@ -286,7 +306,22 @@ class CreateFacultyWindow(Widget):
         self.clear_widgets()
         self.add_widget(FacultyRecordsWindow())
 
+class DailyListButton(ListItemButton):
+    pass
 class DailyAttendanceWindow(Widget):
+    daily_list = ObjectProperty()
+    def view_date(self):
+        del self.daily_list.adapter.data[:]
+        all_daily = DailyAttendance.query.all()
+        for daily in all_daily:
+            #get faculty name
+            for faculty in session.query(Faculty).filter_by(faculty_id=daily.faculty_id):
+                first_name = faculty.first_name
+                middle_name = faculty.middle_name
+                last_name = faculty.last_name
+            details = [str(daily.faculty_id),  first_name+' '+middle_name+' '+last_name, str(daily.is_absent), str(daily.time_in), str(daily.time_out), str(daily.minutes_late)]
+            self.daily_list.adapter.data.extend([", ".join(details)])
+        self.daily_list._trigger_reset_populate()
     def main_menu(self, *args):
         self.clear_widgets()
         self.add_widget(MainMenuWindow())
