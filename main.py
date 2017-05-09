0376c8d3622df00deef7387151099f96c2bce512
Eugene Kasilag <eugene_dose@yahoo.com>
2017-05-09 15:57:55 +0800

    input validation create student

--- main.py
+++ main.py
@@ -391,57 +391,60 @@ class StudentRecordsWindow(Widget):
 
 class CreateStudentWindow(Widget):
     def create(self, *args):
-        student.nickname = self.ids.nickname
-        nickname_text = student.nickname.text
+    	if (self.ids.first_name.text != "" and self.ids.last_name.text != "" and self.ids.address.text != "" and self.ids.sex.text != "" and 
+    		self.ids.guardianA.text != "" and self.ids.contactA.text != "" and self.ids.up_dependent.text != "" and self.ids.nickname.text != "" and
+    		self.ids.middle_name.text != "" and len(self.ids.birth_date.text.split('/')) == 3 and len(self.ids.date_of_admission.text.split('/')) == 3):
+	        student.nickname = self.ids.nickname
+	        nickname_text = student.nickname.text
 
-        student.first_name = self.ids.first_name
-        first_name_text = student.first_name.text
+	        student.first_name = self.ids.first_name
+	        first_name_text = student.first_name.text
 
-        student.middle_name = self.ids.middle_name
-        middle_name_text = student.middle_name.text
+	        student.middle_name = self.ids.middle_name
+	        middle_name_text = student.middle_name.text
 
-        student.last_name = self.ids.last_name
-        last_name_text = student.last_name.text
+	        student.last_name = self.ids.last_name
+	        last_name_text = student.last_name.text
 
-        student.suffix = self.ids.suffix
-        suffix_text = student.suffix.text
+	        student.suffix = self.ids.suffix
+	        suffix_text = student.suffix.text
 
-        student.address = self.ids.address
-        address_text = student.address.text
+	        student.address = self.ids.address
+	        address_text = student.address.text
 
-        student.birth_date = self.ids.birth_date
-        birth_date_text = student.birth_date.text
+	        student.birth_date = self.ids.birth_date
+	        birth_date_text = student.birth_date.text
 
-        student.sex = self.ids.sex
-        sex_text = student.sex.text
+	        student.sex = self.ids.sex
+	        sex_text = student.sex.text
 
-        student.date_of_admission = self.ids.date_of_admission
-        date_of_admission_text = student.date_of_admission.text
+	        student.date_of_admission = self.ids.date_of_admission
+	        date_of_admission_text = student.date_of_admission.text
 
-        #student.group = self.ids.group
-        #group_text = student.group.text
+	        #student.group = self.ids.group
+	        #group_text = student.group.text
 
-        student.guardian1_name = self.ids.guardianA
-        guardian1_name_text = student.guardian1_name.text
+	        student.guardian1_name = self.ids.guardianA
+	        guardian1_name_text = student.guardian1_name.text
 
-        student.guardian2_name = self.ids.guardianB
-        guardian2_name_text = student.guardian2_name.text
+	        student.guardian2_name = self.ids.guardianB
+	        guardian2_name_text = student.guardian2_name.text
 
-        student.contact_number1 = self.ids.contactA
-        contact_number1_text = student.contact_number1.text
+	        student.contact_number1 = self.ids.contactA
+	        contact_number1_text = student.contact_number1.text
 
-        student.contact_number2 = self.ids.contactB
-        contact_number2_text = student.contact_number2.text
+	        student.contact_number2 = self.ids.contactB
+	        contact_number2_text = student.contact_number2.text
 
-        student.up_dependent = self.ids.up_dependent
-        up_dependent_text = student.up_dependent.text
+	        student.up_dependent = self.ids.up_dependent
+	        up_dependent_text = student.up_dependent.text
 
-        new_student = Students(nickname=nickname_text, first_name=first_name_text, middle_name=middle_name_text, last_name=last_name_text, suffix=suffix_text, address=address_text, birth_date=birth_date_text, sex=sex_text, date_of_admission=date_of_admission_text, guardian1_name=guardian1_name_text, guardian2_name=guardian2_name_text, contact_number1=contact_number1_text, contact_number2=contact_number2_text, up_dependent=up_dependent_text)
-        print( add_db(new_student) )
+	        new_student = Students(nickname=nickname_text, first_name=first_name_text, middle_name=middle_name_text, last_name=last_name_text, suffix=suffix_text, address=address_text, birth_date=birth_date_text, sex=sex_text, date_of_admission=date_of_admission_text, guardian1_name=guardian1_name_text, guardian2_name=guardian2_name_text, contact_number1=contact_number1_text, contact_number2=contact_number2_text, up_dependent=up_dependent_text)
+	        print( add_db(new_student) )
 
-        #print(session.query(Students).all())
-        self.clear_widgets()
-        self.add_widget(StudentRecordsWindow())
+	        #print(session.query(Students).all())
+	        self.clear_widgets()
+	        self.add_widget(StudentRecordsWindow())
 
     def back_to_student_records(self, *args):
         self.clear_widgets()
@@ -468,11 +471,14 @@ class EditStudentWindow(Widget):
             self.ids.up_dependent.text = student.up_dependent
 
     def save(self):
+    	if (self.ids.first_name.text != "" and self.ids.last_name.text != "" and self.ids.address.text != "" and self.ids.sex.text != "" and 
+    		self.ids.guardianA.text != "" and self.ids.contactA.text != "" and self.ids.up_dependent.text != "" and self.ids.nickname.text != "" and
+    		self.ids.middle_name.text != "" and len(self.ids.birth_date.text.split('/')) == 3 and len(self.ids.date_of_admission.text.split('/')) == 3):
         #update db for students
-        session.query(Students).filter_by(student_id=studentid).update(dict(nickname=self.ids.nickname.text, first_name=self.ids.first_name.text, middle_name=self.ids.middle_name.text, last_name=self.ids.last_name.text, suffix=self.ids.suffix.text, address=self.ids.address.text, birth_date=self.ids.birth_date.text, sex=self.ids.sex.text, date_of_admission=self.ids.date_of_admission.text, guardian1_name=self.ids.guardianA.text, guardian2_name=self.ids.guardianB.text, contact_number1=self.ids.contactA.text, contact_number2=self.ids.contactB.text, up_dependent=self.ids.up_dependent.text))
-        session.commit()
-        self.clear_widgets()
-        self.add_widget(StudentRecordsWindow())
+	        session.query(Students).filter_by(student_id=studentid).update(dict(nickname=self.ids.nickname.text, first_name=self.ids.first_name.text, middle_name=self.ids.middle_name.text, last_name=self.ids.last_name.text, suffix=self.ids.suffix.text, address=self.ids.address.text, birth_date=self.ids.birth_date.text, sex=self.ids.sex.text, date_of_admission=self.ids.date_of_admission.text, guardian1_name=self.ids.guardianA.text, guardian2_name=self.ids.guardianB.text, contact_number1=self.ids.contactA.text, contact_number2=self.ids.contactB.text, up_dependent=self.ids.up_dependent.text))
+	        session.commit()
+	        self.clear_widgets()
+	        self.add_widget(StudentRecordsWindow())
 
     def back(self):
         self.clear_widgets()
