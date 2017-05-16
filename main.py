fcb7aa9b0d6363907b7a0c6ca3f939f8bd3b4c8d
Eugene Kasilag <eugene_dose@yahoo.com>
2017-05-16 10:12:35 +0800

    create student input validation

--- main.py
+++ main.py
@@ -391,60 +391,79 @@ class StudentRecordsWindow(Widget):
 
 class CreateStudentWindow(Widget):
     def create(self, *args):
-    	if (self.ids.first_name.text != "" and self.ids.last_name.text != "" and self.ids.address.text != "" and self.ids.sex.text != "" and 
-    		self.ids.guardianA.text != "" and self.ids.contactA.text != "" and self.ids.up_dependent.text != "" and self.ids.nickname.text != "" and
-    		self.ids.middle_name.text != "" and len(self.ids.birth_date.text.split('/')) == 3 and len(self.ids.date_of_admission.text.split('/')) == 3):
-	        student.nickname = self.ids.nickname
-	        nickname_text = student.nickname.text
+        student.nickname = self.ids.nickname
+        nickname_text = student.nickname.text
 
-	        student.first_name = self.ids.first_name
-	        first_name_text = student.first_name.text
+        student.first_name = self.ids.first_name
+        first_name_text = student.first_name.text
 
-	        student.middle_name = self.ids.middle_name
-	        middle_name_text = student.middle_name.text
+        student.middle_name = self.ids.middle_name
+        middle_name_text = student.middle_name.text
 
-	        student.last_name = self.ids.last_name
-	        last_name_text = student.last_name.text
+        student.last_name = self.ids.last_name
+        last_name_text = student.last_name.text
 
-	        student.suffix = self.ids.suffix
-	        suffix_text = student.suffix.text
+        student.suffix = self.ids.suffix
+        suffix_text = student.suffix.text
 
-	        student.address = self.ids.address
-	        address_text = student.address.text
+        student.address = self.ids.address
+        address_text = student.address.text
 
-	        student.birth_date = self.ids.birth_date
-	        birth_date_text = student.birth_date.text
+        student.birth_date = self.ids.birth_date
+        birth_date_text = student.birth_date.text
 
-	        student.sex = self.ids.sex
-	        sex_text = student.sex.text
+        student.sex = self.ids.sex
+        sex_text = student.sex.text
 
-	        student.date_of_admission = self.ids.date_of_admission
-	        date_of_admission_text = student.date_of_admission.text
+        student.date_of_admission = self.ids.date_of_admission
+        date_of_admission_text = student.date_of_admission.text
 
-	        #student.group = self.ids.group
-	        #group_text = student.group.text
+        #student.group = self.ids.group
+        #group_text = student.group.text
 
-	        student.guardian1_name = self.ids.guardianA
-	        guardian1_name_text = student.guardian1_name.text
+        student.guardian1_name = self.ids.guardianA
+        guardian1_name_text = student.guardian1_name.text
 
-	        student.guardian2_name = self.ids.guardianB
-	        guardian2_name_text = student.guardian2_name.text
+        student.guardian2_name = self.ids.guardianB
+        guardian2_name_text = student.guardian2_name.text
 
-	        student.contact_number1 = self.ids.contactA
-	        contact_number1_text = student.contact_number1.text
+        student.contact_number1 = self.ids.contactA
+        contact_number1_text = student.contact_number1.text
 
-	        student.contact_number2 = self.ids.contactB
-	        contact_number2_text = student.contact_number2.text
+        student.contact_number2 = self.ids.contactB
+        contact_number2_text = student.contact_number2.text
 
-	        student.up_dependent = self.ids.up_dependent
-	        up_dependent_text = student.up_dependent.text
-
+        student.up_dependent = self.ids.up_dependent
+        up_dependent_text = student.up_dependent.text
+        bday = birth_date_text.split('/')
+        admit = date_of_admission_text.split('/')
+        bday_= 1
+        admit_ = 1 
+        for x in bday: 
+            if (not x.isdigit()):
+                bday_ = 0
+        for x in admit:
+            if (not x.isdigit()):
+                admit_ = 0
+        label = self.ids.error
+        if (nickname_text != "" and first_name_text != "" and middle_name_text != "" and last_name_text != "" and
+            address_text != "" and len(bday) == 3 and len(admit) == 3 and bday_ == 1 and admit_ == 1 and sex_text != "" and
+            guardian1_name_text != "" and int(int(contact_number1_text) / 1000000000) == 9 and contact_number1_text.isdigit() and
+            up_dependent_text != ""): 
 	        new_student = Students(nickname=nickname_text, first_name=first_name_text, middle_name=middle_name_text, last_name=last_name_text, suffix=suffix_text, address=address_text, birth_date=birth_date_text, sex=sex_text, date_of_admission=date_of_admission_text, guardian1_name=guardian1_name_text, guardian2_name=guardian2_name_text, contact_number1=contact_number1_text, contact_number2=contact_number2_text, up_dependent=up_dependent_text)
 	        print( add_db(new_student) )
 
 	        #print(session.query(Students).all())
 	        self.clear_widgets()
 	        self.add_widget(StudentRecordsWindow())
+        elif (nickname_text == "" or first_name_text == "" or middle_name_text == "" or last_name_text == "" or address_text == "" or sex_text == "" or 
+            guardian1_name_text == "" or contact_number1_text == "" or up_dependent_text == "" or birth_date_text == "" or date_of_admission_text == ""):
+            label.text = "*This field is required"
+        elif (len(admit) != 3 or len(bday) != 3 or bday_ != 1 or admit_ != 1):
+            label.text = "Date should be in MM/DD/YY format"
+        elif (contact_number1_text != ""):
+            if (int(int(contact_number1_text) / 1000000000) != 9):
+                label.text = "Contact number should be in 09xxxxxxxxx format"
 
     def back_to_student_records(self, *args):
         self.clear_widgets()
